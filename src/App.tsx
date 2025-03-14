41fe8a7a4ac544f84dc2c209f85a63a0392118da
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-13 21:33:40 -0300

    criando e adicionando o App.tsx e componentes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header/Header.tsx'
+import Home from './pages/Home/'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  )
+}
