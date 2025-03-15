34a8a50644e5054326f3e3021644ea647c40177d
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-14 22:13:57 -0300

    :sparkles: feat: visionamento semantico

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-
+import './GlobalStyle/GlobalStyle.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
