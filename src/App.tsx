0e851420d4a7f1c8fd460bd1194e69f6b825e677
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-14 22:12:53 -0300

    :sparkles: feat: visionamento semantico

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import Header from './components/Header/Header.tsx'
-import Home from './pages/Home/'
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-    </>
-  )
+  return <Header />
 }
