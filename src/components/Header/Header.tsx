b44d38f1e35b0419ca7650608c1bbe0363436013
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-17 17:42:33 -0300

    refactor: adiciona constantes para melhorar organização e melhora a estrutura do BrowserRouter, organização do código e rotas

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import s from './Header.module.scss'
 import logoSales from '../../assets/logo_salesmeet.png'
 import Home from '../../pages/Home/Home'
+import Footer from '../Footer/Footer'
 
 export default function Header() {
   return (
@@ -16,20 +17,29 @@ export default function Header() {
             />
           </section>
           <section className={s.sessaoNav}>
-            <ul>
+            <ul className={s.uldaNav}>
               <li>
                 <Link className={s.Home} to='/'>
                   Home
                 </Link>
               </li>
-              <li></li>
-              <li></li>
+              <li>
+                <Link className={s.DetalhesdoEvento} to=''>
+                  Detalhes do Evento
+                </Link>
+              </li>
+              <li>
+                <Link className={s.Contato} to='/Footer'>
+                  Contato
+                </Link>
+              </li>
             </ul>
           </section>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Footer' element={<Footer />} />
       </Routes>
     </BrowserRouter>
   )
