ffeca6ffe3d0173a97d51b9aa4c71deaa1032c1c
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-17 17:35:04 -0300

    feat:  adiciona descrição,  data e detalhes nos cards de evento

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,16 +1,43 @@
+// import { Link } from 'react-router-dom'
+// import { GoArrowRight } from 'react-icons/go'
+
+import s from './EventCard.module.scss'
+
 type EventCardProps = {
-  name: string
+  id: number
+  title: string
   date: Date | number
   description: string
+  location: string
+  imageSrc: string
 }
 
 export default function EventCard(props: EventCardProps) {
-  const eventName = props.name
-  const eventDate = props.date
-  const eventDescription = props.description
+  const { title, date, location, imageSrc } = props
+
+  const day = new Date(date).getUTCDate()
+  const month = new Date(date).toLocaleString('pt-BR', { month: 'short' })
+
   return (
-    <div>
-      {eventName},{`${eventDate}`}, {eventDescription}
+    <div className={s.event}>
+      <div className={s.event_header}>
+        <div className={s.event_image_container}>
+          <img
+            src={imageSrc}
+            alt='Imagem do evento'
+            className={s.event_image}
+          />
+        </div>
+        <div className={s.event_date}>
+          <span className={s.event_date_day}>{day}</span>
+          <span className={s.event_date_month}>{month}</span>
+        </div>
+      </div>
+      <div className={s.event_body}>
+        <h4 className={s.event_title}>{title}</h4>
+        <p className={s.event_location}>{location}</p>
+        {/* <p className='event-description'>{description}</p> */}
+      </div>
     </div>
   )
 }
