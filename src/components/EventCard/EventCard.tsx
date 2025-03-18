5efa44c51ac6ca211fcc48831bd964955b66f033
Danilo Almeida <danilo.laet52@gmail.com>
2025-03-18 19:24:37 -0300

    feat: adiciona direcionamento nos cards e refatora estrutura do código

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,6 +1,4 @@
-// import { Link } from 'react-router-dom'
-// import { GoArrowRight } from 'react-icons/go'
-
+import { Link } from 'react-router-dom'
 import s from './EventCard.module.scss'
 
 type EventCardProps = {
@@ -13,31 +11,33 @@ type EventCardProps = {
 }
 
 export default function EventCard(props: EventCardProps) {
-  const { title, date, location, imageSrc } = props
+  const { id, title, date, location, imageSrc } = props
 
   const day = new Date(date).getUTCDate()
   const month = new Date(date).toLocaleString('pt-BR', { month: 'short' })
 
   return (
-    <div className={s.event}>
-      <div className={s.event_header}>
-        <div className={s.event_image_container}>
-          <img
-            src={imageSrc}
-            alt='Imagem do evento'
-            className={s.event_image}
-          />
-        </div>
-        <div className={s.event_date}>
-          <span className={s.event_date_day}>{day}</span>
-          <span className={s.event_date_month}>{month}</span>
-        </div>
-      </div>
-      <div className={s.event_body}>
-        <h4 className={s.event_title}>{title}</h4>
-        <p className={s.event_location}>{location}</p>
-        {/* <p className='event-description'>{description}</p> */}
-      </div>
-    </div>
+    <Link to={`/evento/${id}`} className={s.eventlink}>
+      <section className={s.event}>
+        <section className={s.event_header}>
+          <figure className={s.event_image_container}>
+            <img
+              src={imageSrc}
+              alt='Imagem fazendo referencia ao evento'
+              className={s.event_image}
+            />
+          </figure>
+          <time className={s.event_date}>
+            <span className={s.event_date_day}>{day}</span>
+            <span className={s.event_date_month}>{month}</span>
+          </time>
+        </section>
+        <article className={s.event_body}>
+          <h4 className={s.event_title}>{title}</h4>
+          <p className={s.event_location}>{location}</p>
+          {/* <p className='event_description'>{description}</p> */}
+        </article>
+      </section>
+    </Link>
   )
 }
